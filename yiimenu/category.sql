4847da3bd12a815b08d73f0912ed8ef82c34d2fa
maxym16 <goolyamaxym@gmail.com>
2016-12-28 11:58:05 +0200

    Creating a recursive menu

---
+++ yiimenu/category.sql
@@ -0,0 +1,95 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Час створення: Гру 28 2016 р., 10:10
+-- Версія сервера: 10.1.10-MariaDB
+-- Версія PHP: 7.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База даних: `yiimenu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `parent_id` int(10) UNSIGNED DEFAULT '0',
+  `name` varchar(255) NOT NULL,
+  `keywords` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп даних таблиці `category`
+--
+
+INSERT INTO `category` (`id`, `parent_id`, `name`, `keywords`, `description`) VALUES
+(1, 0, 'Sportswear', NULL, NULL),
+(2, 0, 'Mens', NULL, NULL),
+(3, 0, 'Womens', NULL, NULL),
+(4, 1, 'Nike', '', ''),
+(5, 1, 'Under Armour', NULL, NULL),
+(6, 1, 'Adidas', NULL, NULL),
+(7, 1, 'Puma', NULL, NULL),
+(8, 1, 'ASICS', NULL, NULL),
+(9, 2, 'Fendi', NULL, NULL),
+(10, 2, 'Guess', NULL, NULL),
+(11, 2, 'Valentino', NULL, NULL),
+(12, 2, 'Dior', NULL, NULL),
+(13, 2, 'Versace', NULL, NULL),
+(14, 2, 'Armani', NULL, NULL),
+(15, 2, 'Prada', NULL, NULL),
+(16, 2, 'Dolce and Gabbana', NULL, NULL),
+(17, 2, 'Chanel', NULL, NULL),
+(18, 2, 'Gucci', NULL, NULL),
+(19, 3, 'Fendi', NULL, NULL),
+(20, 3, 'Guess', NULL, NULL),
+(21, 3, 'Valentino', NULL, NULL),
+(22, 3, 'Dior', NULL, NULL),
+(23, 3, 'Versace', NULL, NULL),
+(24, 0, 'Kids', NULL, NULL),
+(25, 0, 'Fashion', NULL, NULL),
+(26, 0, 'Households', NULL, NULL),
+(27, 0, 'Interiors', NULL, NULL),
+(28, 0, 'Clothing', NULL, NULL),
+(29, 0, 'Bags', 'сумки ключевики...', 'сумки опис...'),
+(30, 0, 'Shoes', NULL, NULL),
+(37, 7, 'Top Puma', 'Puma', 'Puma');
+
+--
+-- Індекси збережених таблиць
+--
+
+--
+-- Індекси таблиці `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для збережених таблиць
+--
+
+--
+-- AUTO_INCREMENT для таблиці `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
